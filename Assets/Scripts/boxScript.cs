7f08df058ced84dde6242d033e5ad5f218162794
annalisegong <annalise.gong@cuw.edu>
2022-04-11 11:38:47 -0500

    navMesh stuff

--- Assets/Scripts/boxScript.cs
+++ Assets/Scripts/boxScript.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class boxScript : MonoBehaviour
 {
     private int count;
+    public GameObject thePlayer;
+    private Vector3 playerPosition;
+    private Rigidbody rb;
+    public float speed = 20f;
+    private NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start() //like a constructor
     {
         count = 0;
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        agent = this.gameObject.GetComponent<NavMeshAgent>();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -28,6 +35,7 @@ public class boxScript : MonoBehaviour
 
     void Update()
     {
-
+        playerPosition = thePlayer.transform.position;
+        rb.velocity = new Vector3(playerPosition.x, 0, playerPosition.z) * speed;
     }
 }
